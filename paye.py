46864e83e49e8a38677c5273136ff01ed4427b23
karenshauri <karennduta12@gmail.com>
2025-09-16 09:44:57 +0300

    paye calculation

---
+++ paye.py
@@ -0,0 +1,19 @@
+def calculate_payee(self):
+    """
+    Calculate PAYE (income tax) based on KRA brackets.
+    Uses gross salary minus NSSF as taxable income.
+    """
+    # First subtract NSSF to get taxable income
+    taxable_income = self.gross_salary - self.calculate_nssf()
+    
+    # Now apply KRA brackets
+    if taxable_income <= 24000:
+        tax = taxable_income * 0.10
+    elif taxable_income <= 32333:
+        # 24,000 taxed at 10% + the rest at 25%
+        tax = (24000 * 0.10) + ((taxable_income - 24000) * 0.25)
+    else:
+        # 24,000 @10% + 8,333 @25% + the rest @30%
+        tax = (24000 * 0.10) + (8333 * 0.25) + ((taxable_income - 32333) * 0.30)
+    
+    return tax
